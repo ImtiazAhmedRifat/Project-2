a91b00902b8533523e55528753a95abb583cf023
ImtiazAhmedRifat <rifatimtiaz105@gmail.com>
2025-01-10 18:56:18 +0600

    finish the project

--- scripts/app.js
+++ scripts/app.js
@@ -8,17 +8,30 @@ const loadPetCategories = async () => {
   displayPetCategories(data.categories);
 };
 
+// show by category
+// / const res = await fetch(`https://openapi.programming-hero.com/api/peddy/category/${id}`);
+  // const data = await res.json();
+  // displayAllPets(data.data)
+  const showPetByCategories = (id) => {
+    console.log(id)
+  }
+
+
 const displayPetCategories = (items) => {
   const loadCategoryContainer = document.getElementById("load-pet-categories");
   items.forEach((item) => {
-    console.log(item);
+    // console.log(item);
 
-    const button = document.createElement("button");
-    button.classList = "btn text-xl border-1 rounded-xl border-gray-300";
-    button.innerText = `${item.category}`;
+    const buttonContainer = document.createElement("div");
+    buttonContainer.classList = "btn text-xl border-1 rounded-xl border-gray-300";
+    buttonContainer.innerHTML= ` 
+    <button onclick="showPetByCategories(${item.category})" class="flex gap"><img class=" w-8 " src="${item.category_icon}"> ${item.category}</button>
+    `;
+    
 
     // add dynamic button
-    loadCategoryContainer.append(button);
+    loadCategoryContainer.append(buttonContainer);
+    // console.log(item.category)
   });
 };
 
@@ -33,13 +46,15 @@ const loadAllPet = async () => {
 
 const displayAllPets = (pets) => {
   const loadVideoContainer = document.getElementById("load-video-container");
+  loadVideoContainer.innerHTML = "";
   pets.forEach( (pet) => {
-    console.log(pet)
+    // console.log(pet)
     const card = document.createElement("div");
-    card.classList = "card  shadow-xl"
+    card.classList = "card  shadow-xl border-2 border-gray-300 rounded-xl"
     card.innerHTML =  `
       <figure>
     <img
+    class="w-96 p-2 rounded-xl"
       src="${pet.image}"
       alt="Shoes" />
   </figure>
@@ -47,12 +62,12 @@ const displayAllPets = (pets) => {
     <h2 class="card-title">
      ${pet.pet_name};
     </h2>
-    <p>Breed: ${pet.breed}</p>
-    <p>Date Of Birth: ${pet?.date_of_birth}</p>
-    <p>Gender: ${pet?.gender}</p>
-    <p>Price: ${pet?.price}</p>
-    <div class="card-actions justify-evenly">
-      <div  class="badge badge-outline"><button onclick="callLikePets()"  class="like-btn">Like</button></div>
+    <p>Breed: ${pet.breed ? pet.breed : "Not Available"}</p>
+    <p>Date Of Birth: ${pet.date_of_birth ? pet.date_of_birth : "Not Available"}</p>
+    <p>Gender: ${pet.gender ? pet.gender : "Not Available"}</p>
+    <p>Price: ${pet.price ? pet.price : "Not Available"}$</p>
+    <div class="card-actions justify-evenl items-center">
+      <div  class=""><button onclick="callLikePets()"  class="btn border-2 border-gray-400">Like</button></div>
       <div class="badge badge-outline"><button class="">Adopt</button></div>
       <div class="badge badge-outline"><button class=""></button>Details</div>
     </div>
@@ -61,16 +76,18 @@ const displayAllPets = (pets) => {
     loadVideoContainer.append(card);
   });
 }
-const callLikePets = (pets) => {
- const likeContainer = document.getElementById("liked-container");
-pets.forEach((pet)=>{
-  console.log(pet)
-  const div = document.createElement("div");
- div.innerText = `${pet.image}`;
- likeContainer.appendChild(div)
-});
  
-
+const callLikePets = (pets) => {
+  const likeContainer = document.getElementById("liked-container")
+  
+ pets.forEach((pet)=>{
+  const div = document.createElement("div");
+  div.classList = "h-1/2 flex justify-center items-center"
+  div.innerHTML = `
+  <img src="${pet.image}"/>
+  `;
+  likeContainer.appendChild(div);
+ })
 }
 // petCategoriesShow()
 loadPetCategories();
